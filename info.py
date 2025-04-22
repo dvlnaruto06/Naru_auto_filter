aed1ab2747928c4a8107b128903ceda9ff7fa167
dvlnaruto06 <koshtiharshil006@gmail.com>
2025-04-22 13:32:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -73,7 +73,7 @@ DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://Narutoautofilter:the_b
 if len(DATABASE_URL) == 0:
     print('Error - DATABASE_URL is missing, exiting now')
     exit()
-SECOND_DATABASE_URL = environ.get('SECOND_DATABASE_URL', "mongodb+srv://Narutoautofilter:the_bad_devil_06@cluster0.glzywkd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+SECOND_DATABASE_URL = environ.get('SECOND_DATABASE_URL', "https://")
 if len(SECOND_DATABASE_URL) == 0:
     print('Info - SECOND_DATABASE_URL is empty')
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
