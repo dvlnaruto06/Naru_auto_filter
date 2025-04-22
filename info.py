bead263483ba3dc80f3ed66dd32c86a98d73acd8
dvlnaruto06 <koshtiharshil006@gmail.com>
2025-04-22 15:08:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -73,7 +73,7 @@ DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://Narutoautofilter:the_b
 if len(DATABASE_URL) == 0:
     print('Error - DATABASE_URL is missing, exiting now')
     exit()
-SECOND_DATABASE_URL = environ.get('SECOND_DATABASE_URL', "0")
+SECOND_DATABASE_URL = environ.get('SECOND_DATABASE_URL', "https://")
 if len(SECOND_DATABASE_URL) == 0:
     print('Info - SECOND_DATABASE_URL is empty')
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
@@ -130,7 +130,7 @@ if len(URL) == 0:
 else:
     if URL.startswith(('https://', 'http://')):
         if not URL.endswith("/"):
-            URL += '/'
+            URL += 'https://'
     elif is_valid_ip(URL):
         URL = f'http://{URL}/'
     else:
